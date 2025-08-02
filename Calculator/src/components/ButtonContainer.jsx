3ff1babc0fea3b0072841eb43b475e14e016b8cb
Tiasha-code <tiashaganguly2005@gmail.com>
2025-08-02 13:19:21 +0530

    commit

---
+++ Calculator/src/components/ButtonContainer.jsx
@@ -0,0 +1,11 @@
+import styles from "./ButtonContainer.module.css";
+
+const ButtonContainer=() => {
+  const buttonNames =['C', '()', '%','/.', '7','8', '9', ' x ','4', '5','6', '-', '1','2', '3', '+','^' ,'0','.','='];
+  return (
+  <div className={styles.buttonsContainer}>
+    {buttonNames.map(buttonName =>  (<button className={styles.button}>{buttonName}</button>))}
+      </div>
+  );
+};
+export default ButtonContainer;
